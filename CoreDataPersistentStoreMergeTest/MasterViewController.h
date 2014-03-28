c115ee2683ca25272433213a693302b7441ceea8
Daniel Tull <dt@danieltull.co.uk>
2014-03-28 10:46:27 +0000

    Initial Commit

---
+++ CoreDataPersistentStoreMergeTest/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  CoreDataPersistentStoreMergeTest
+//
+//  Created by Daniel Tull on 28/03/2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
